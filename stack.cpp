30fc22f663b1c2849124bc596c65d910a622eb39
Isaac Abraham <isaac.abraham@g.austincc.edu>
2026-06-22 04:18:52 +0000

    Initialize stack.cpp with matching header documentation blocks

---
+++ stack.cpp
@@ -0,0 +1,8 @@
+/*********************************************************
+ * Name: Isaac Abraham
+ * Assignment 4: Simple Int Stack
+ *********************************************************/
+
+#include "stack.h"
+
+// We will implement the constructor and member methods next
